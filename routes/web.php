72dd8aa69c69923c6b9c1b0836632fef2a8c3f3a
moisesmtzs <moises.martinez6605@alumnos.udg.mx>
2022-11-30 23:24:52 -0600

    Proyecto terminado. Intento de autenticación con Google, pero hay error por la incorporación con Jetstream

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\CitaController;
 use App\Http\Controllers\ServicioController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,4 +46,29 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/login-google', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-callback', function () {
+    $user = Socialite::driver('google')->user();
+
+    $userExists = User::where('external_id' , $user->id)->where('external_auth', 'google')->first();
+
+    if ( $userExists ) {
+        Auth::login($userExists);
+    } else {
+        $userNew = User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'avatar' => $user->avatar,
+            'password' => '1234567890',
+            'external_id' => $user->id,
+            'external_auth' => 'google'
+        ]);
+        Auth::login($userNew);
+    }
+    return redirect('/dashboard');
 });
